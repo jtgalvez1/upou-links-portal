fb53cfa4ff5a229034ca318e80907999ec059e88
jmarias-edu <jmarias@up.edu.ph>
2023-07-19 16:07:13 +0800

    feat: Adding of new user types

--- app/api/user/router.py
+++ app/api/user/router.py
@@ -57,3 +57,14 @@ def bookmark(userid, link_id):
       'message'     : 'Successfully updated bookmark',
       'action'      : action
    })
+
+@user.route('/addType', methods=["POST"])
+def addType():
+   form = request.form.to_dict()
+   newname = form['usertypename']
+   action = add_user_type(newname)
+   return jsonify({
+      'status'      : 200,
+      'message'     : 'Successfully updated bookmark',
+      'action'      : action
+   })
