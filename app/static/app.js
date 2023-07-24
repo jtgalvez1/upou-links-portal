97e58b01abe3e28bbee509e49513ec409dea69f4
Miguel Galvez <jtgalvez1@up.edu.ph>
2023-07-24 16:13:04 +0800

    css: pwa install popup do not show again

--- app/static/app.js
+++ app/static/app.js
@@ -1,52 +1,59 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-    .register('/service-worker.js')
-    .then(function(registration) {
-        console.log('Service Worker Registered');
-        return registration;
-    })
-    .catch(function(err) {
-        console.error('Unable to register service worker.', err);
-    });
-}
+document.addEventListener('DOMContentLoaded', async () => {
+  let showPWA = await localStorage.getItem('showPWA') || true;
 
-let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-const pwaModal = document.querySelector('#pwa.modal')
-// addBtn.style.display = 'none';
-pwaModal.style.display = 'none';
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-  // addBtn.style.display = 'block';
-  pwaModal.style.display = 'block'
-  addBtn.addEventListener('click', (e) => {
-    // addBtn.style.display = 'none';
-    pwaModal.style.display = 'none';
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-
-
-          console.log('User accepted the A2HS prompt');
-
-
-        } else {
-
-          console.log('User dismissed the A2HS prompt');
-
-        }
-        deferredPrompt = null;
+  if (showPWA === true) {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(function(registration) {
+          console.log('Service Worker Registered');
+          return registration;
+      })
+      .catch(function(err) {
+          console.error('Unable to register service worker.', err);
       });
-  });
-  setTimeout( () => pwaModal.style.display = 'none', 5000) 
-});
+    }
 
-window.addEventListener('online', function(e) {
-    console.log("You are online");
-}, false);
+    let deferredPrompt;
+    const addBtn = document.querySelector('.add-button');
+    const pwaModal = document.querySelector('#pwa.modal')
+    pwaModal.style.top = '6em';
+    setTimeout( () => pwaModal.style.top = '-20em', 5000) 
 
-document.querySelector('#pwa.modal > svg').addEventListener('click', ev => {
-  pwaModal.style.display = 'none';
+    window.addEventListener('beforeinstallprompt', (e) => {
+      e.preventDefault();
+      deferredPrompt = e;
+      addBtn.addEventListener('click', (e) => {
+      pwaModal.style.top = '-20em';
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+
+
+            console.log('User accepted the A2HS prompt');
+
+
+          } else {
+
+            console.log('User dismissed the A2HS prompt');
+
+          }
+          deferredPrompt = null;
+        });
+    });
+    });
+
+    window.addEventListener('online', function(e) {
+      console.log("You are online");
+    }, false);
+
+    document.querySelector('#pwa.modal > svg').addEventListener('click', ev => {
+      pwaModal.style.top = '-20em';
+    })
+
+    document.querySelector('#never-again').addEventListener('click', ev => {
+      pwaModal.style.top = '-20em';
+    localStorage.setItem('showPWA', false);
+    })
+  }
 })
