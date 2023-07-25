dcce632434c948cbae49cc48fc2e3efdf6755140
Miguel Galvez <jtgalvez1@up.edu.ph>
2023-07-25 11:46:22 +0800

    feat: dark mode toggle

--- app/static/app.js
+++ app/static/app.js
@@ -1,4 +1,44 @@
 document.addEventListener('DOMContentLoaded', async () => {
+
+  let root = document.querySelector(':root');
+  let userTheme = localStorage.getItem('userTheme') 
+                ? localStorage.getItem('userTheme') 
+                : (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) 
+                ? 'dark' 
+                : 'light';
+  let darkModeToggle = document.querySelector('#dark-mode')
+
+  changeTheme(userTheme)
+
+  darkModeToggle.addEventListener('change', ev => {
+    let theme = ev.currentTarget.checked ? 'dark' : 'light';
+    changeTheme(theme)
+    localStorage.setItem('userTheme', theme)
+  })
+
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    changeTheme(localStorage.getItem('userTheme') == null && event.matches ? "dark" : "light");
+  });
+
+
+  function changeTheme(theme) {
+    if (theme === 'dark') {
+      root.style.setProperty('--bg-color', '#2c2c2c')
+      root.style.setProperty('--font-color', '#cdcdcd')
+      root.style.setProperty('--card-bg', '#cdcdcd')
+      root.style.setProperty('--link', 'blue')
+      root.style.setProperty('--visited-link', 'purple')
+      darkModeToggle.checked = true;
+    } else if (theme === 'light') {
+      root.style.setProperty('--bg-color', '#cdcdcd')
+      root.style.setProperty('--font-color', '#333')
+      root.style.setProperty('--card-bg', '#2c2c2c')
+      root.style.setProperty('--link', 'skyblue')
+      root.style.setProperty('--visited-link', '#aa7799')
+      darkModeToggle.checked = false;
+    }
+  }
+
   let showPWA = await localStorage.getItem('showPWA') || true;
 
   if (showPWA === true) {
@@ -56,24 +96,59 @@ document.addEventListener('DOMContentLoaded', async () => {
     localStorage.setItem('showPWA', false);
     })
   }
+})
 
-  let root = document.querySelector(':root');
-  let userTheme = localStorage.getItem('userTheme') || null;
 
-  if (userTheme != null) {
-    if (userTheme === 'dark') {
-      root.style.setProperty('--bg-color', '#2c2c2c')
-      root.style.setProperty('--font-color', '#cdcdcd')
-      root.style.setProperty('--card-bg', '#cdcdcd')
-      root.style.setProperty('--link', 'blue')
-      root.style.setProperty('--visited-link', 'purple')
-    } else if (userTheme === 'light') {
-      root.style.setProperty('--bg-color', '#cdcdcd')
-      root.style.setProperty('--font-color', '#333')
-      root.style.setProperty('--card-bg', '#2c2c2c')
-      root.style.setProperty('--link', 'skyblue')
-      root.style.setProperty('--visited-link', '#aa7799')
-    }
+// sidebar script
+
+document.querySelector('#close.sidebar').addEventListener('click', ev =>{
+  ev.preventDefault();
+  document.querySelector('#sidebar').style.left = '-100vw';
+})
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+return  evt.touches;
+}                                                     
+                                                                      
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];                                      
+  xDown = firstTouch.clientX;                                      
+  yDown = firstTouch.clientY;                                      
+};                                                
+                                                                      
+function handleTouchMove(evt) {
+  if ( ! xDown || ! yDown ) {
+      return;
   }
 
-})
+  var xUp = evt.touches[0].clientX;                                    
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+                                                                      
+  if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+      if ( xDiff > 0 ) {
+          /* right swipe */ 
+          document.querySelector('#sidebar').style.left = '-100vw';
+      } else {
+          /* left swipe */
+          document.querySelector('#sidebar').style.left = '0';
+      }                       
+  } else {
+      if ( yDiff > 0 ) {
+          /* down swipe */ 
+      } else { 
+          /* up swipe */
+      }                                                                 
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;                                             
+};
