e136184db18f2a4ddf8938d39e453c5ad5c09859
jmarias-edu <jmarias@up.edu.ph>
2023-08-16 09:45:40 +0800

    feat: changes in config file for cookie expiry

--- app/configs.py
+++ app/configs.py
@@ -1,4 +1,8 @@
+import datetime
+
 BASE_URL = 'http://localhost:5000'
 DB_PATH = './app/database/'
 IMAGES_PATH = './app/static/images/'
 SESSION_TYPE = 'filesystem'
+SESSION_PERMANENT = True
+PERMANENT_SESSION_LIFETIME = datetime.timedelta(hours=6)
